282f256ac1fb3da093a5c22e73b9ae0e1eac8724
LeonardoLopesPrado <111136996+LeonardoLopesPrado@users.noreply.github.com>
2024-05-15 17:27:19 -0300

    instance type change

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "public_instance" {
   ami               = "ami-04b70fa74e45c3917"
-  insance_type      = "t2.medium"
+  instance_type      = "t2.medium"
   subnet_id         = aws_subnet.subnet-publica.id 
 
   tags = {
