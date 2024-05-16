835353e2f4e17f7ee519b2ceb558b9686a2dfb32
LeonardoLopesPrado <111136996+LeonardoLopesPrado@users.noreply.github.com>
2024-05-16 20:36:12 -0300

    Alterado acl corretamente

--- acl.tf
+++ acl.tf
@@ -1,5 +1,6 @@
-resource "aws_network_acl" "main" {
+resource "aws_network_acl" "acl_publica" {
   vpc_id = aws_vpc.vpc-01.id
+  subnet_ids = [aws_subnet.subnet-publica.id]
 
   // Egress rules
   egress {
@@ -50,7 +51,7 @@ resource "aws_network_acl" "main" {
   // Ingress rules
   ingress {
     protocol   = "tcp"
-    rule_no    = 100
+    rule_no    = 101
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 443
@@ -59,7 +60,7 @@ resource "aws_network_acl" "main" {
 
   ingress {
     protocol   = "tcp"
-    rule_no    = 200
+    rule_no    = 201
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 8080
@@ -68,7 +69,7 @@ resource "aws_network_acl" "main" {
 
   ingress {
     protocol   = "tcp"
-    rule_no    = 300
+    rule_no    = 301
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 22
@@ -77,7 +78,7 @@ resource "aws_network_acl" "main" {
 
   ingress {
     protocol   = "tcp"
-    rule_no    = 400
+    rule_no    = 401
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 3306
@@ -86,7 +87,110 @@ resource "aws_network_acl" "main" {
 
   ingress {
     protocol   = "tcp"
+    rule_no    = 501
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+
+resource "aws_network_acl" "acl_privada" {
+  vpc_id = aws_vpc.vpc-01.id
+  subnet_ids = [aws_subnet.subnet-privada.id]
+
+  // Egress rules
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 8080
+    to_port    = 8080
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 3306
+    to_port    = 3306
+  }
+
+  egress {
+    protocol   = "tcp"
     rule_no    = 500
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  // Ingress rules
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 101
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 201
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 8080
+    to_port    = 8080
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 301
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 401
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 3306
+    to_port    = 3306
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 501
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 80
